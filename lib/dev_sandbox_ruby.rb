661857b07ae010d83cf0f79934506e668f0f58c4
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2022-09-13 14:59:13 +0700

    fix: update .gemspec

--- lib/dev_sandbox_ruby.rb
+++ lib/dev_sandbox_ruby.rb
@@ -1,8 +1,28 @@
 # frozen_string_literal: true
 
+require "openssl"
+require "base64"
+require "json"
+
 require_relative "dev_sandbox_ruby/version"
 
 module DevSandboxRuby
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.create_content_signature(timestamp, payload, partner_privkey_path)
+    privkey = OpenSSL::PKey::RSA.new(File.read(partner_privkey_path))
+    digest = OpenSSL::Digest::SHA256.new
+    data = timestamp + JSON.parse(payload.read).to_json
+    signature = privkey.sign(digest, data)
+    content_signature = Base64.encode64(signature)
+    return content_signature
+  end
+
+  def self.verify_content_signature(encode_signature, data, truemoney_pubkey_path)
+    pubkey = OpenSSL::PKey::RSA.new(File.read(truemoney_pubkey_path))
+    digest = OpenSSL::Digest::SHA256.new
+    signature = Base64.decode64(encode_signature)
+    result = pubkey.verify(digest, signature, data)
+    return result
+  end
 end
