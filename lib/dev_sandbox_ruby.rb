a09c6824d120fa7291bbc780ac64e7ee5e035d3a
Punnarat Rattanapawan <punnarat_mos@hotmail.com>
2022-09-14 13:40:58 +0700

    fix: json

--- lib/dev_sandbox_ruby.rb
+++ lib/dev_sandbox_ruby.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
 
-require "openssl"
-require "base64"
-require "json"
-
 require_relative "dev_sandbox_ruby/version"
 
 module DevSandboxRuby
   class Error < StandardError; end
+  class InvalidRequestHeaderError < Error; end
 
   def self.create_content_signature(timestamp, payload, partner_privkey_path)
     privkey = OpenSSL::PKey::RSA.new(File.read(partner_privkey_path))
     digest = OpenSSL::Digest::SHA256.new
-    data = timestamp + JSON.parse(payload.read).to_json
+    data = timestamp + JSON.generate(payload)
     signature = privkey.sign(digest, data)
-    content_signature = Base64.encode64(signature)
-    return content_signature
+    content_signature = Base64.strict_encode64(signature)
+    return "digest-alg=RSA-SHA; key-id=KEY:RSA:rsf.org; data=#{content_signature}"
   end
 
-  def self.verify_content_signature(encode_signature, data, truemoney_pubkey_path)
+  def self.verify_content_signature(timestamp, content_signature, payload, truemoney_pubkey_path)
     pubkey = OpenSSL::PKey::RSA.new(File.read(truemoney_pubkey_path))
     digest = OpenSSL::Digest::SHA256.new
-    signature = Base64.decode64(encode_signature)
+    unless content_signature.match(/digest-alg=RSA-SHA; key-id=KEY:RSA:rsf.org; data=(\S+)/)
+      raise InvalidRequestHeaderError, "Invalid Content-Signature header"
+    end
+    encoded_signature = content_signature.match(/digest-alg=RSA-SHA; key-id=KEY:RSA:rsf.org; data=(\S+)/)[1]
+    signature = Base64.decode64(encoded_signature)
+    data = timestamp + JSON.generate(payload).to_json
     result = pubkey.verify(digest, signature, data)
     return result
   end
